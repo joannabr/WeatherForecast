dbce211886cf58057d0db0d886bffc67609a665d
AsiaB <joanna.brania@vp.pl>
2018-08-16 10:57:41 +0200

    1 change

--- src/main/java/pl/joannabrania/weathermap/models/Config.java
+++ src/main/java/pl/joannabrania/weathermap/models/Config.java
@@ -6,5 +6,4 @@ public class Config {
     public static final String URL_TO_API2="https://samples.openweathermap.org/data/2.5/forecast?q=";
 
     public static final String API_KEY = "13436fe49b7a896be455efac50f09c87";
-
-    }
+}
